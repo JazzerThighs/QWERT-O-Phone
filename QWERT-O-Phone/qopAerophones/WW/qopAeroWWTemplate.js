791ac4a78f2c66385ee310e2bef32cb96e272573
JazzerThighs <104684457+JazzerThighs@users.noreply.github.com>
2023-02-11 16:06:45 -0500

    Add files via upload

---
+++ QWERT-O-Phone/qopAerophones/WW/qopAeroWWTemplate.js
@@ -0,0 +1,19 @@
+const qopAeroWWTemplate = {
+    name : "qopAeroWWDefault",
+    transposition : 0,
+    numberOfFingers : 8,
+    fingeringList : [
+        {
+        NoteIndex : 0,
+        NoteName : "C4",
+        FingerString : [false, false, false, false, false, false],
+        FingerStringLength : 1
+        },
+        {
+        NoteIndex : 1,
+        NoteName : "C#/Db4",
+        FingerString : [true, true, true, false, false, false],
+        FingerStringLength : 3
+        },
+    ]
+}
