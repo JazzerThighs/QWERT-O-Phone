5aefc36c74aa7965a6de03450c45ce19a877ee1a
JazzerThighs <profenjb199@gmail.com>
2023-08-12 18:53:08 -0400

    modified:   create-a-qwert-o-phone/src/UserDataMode/importExportUD.ts

--- create-a-qwert-o-phone/src/UserDataMode/importExportUD.ts
+++ create-a-qwert-o-phone/src/UserDataMode/importExportUD.ts
@@ -6,14 +6,14 @@ export function DownloadQOPUDJSON(obj: QOPUserDataTemplate) {
 	if (obj.Timestamp !== undefined) {
 		obj.Timestamp = formattedDateTime;
 	}
-	const jsonString = JSON.stringify(obj, null, 2); // Convert object to JSON string
-	const blob = new Blob([jsonString], { type: 'application/json' }); // Create a Blob out of the string
-	const downloadLink = document.createElement('a'); // Create a download link for the blob
-	downloadLink.href = URL.createObjectURL(blob); // Use the Object URL as the href for the download link
-	downloadLink.download = `QOPUserData${formattedDateTime}.json`; // Set the download attribute of the link to give the desired name to the file
-	document.body.appendChild(downloadLink); // Append the link to the body
-	downloadLink.click(); // Programmatically trigger a click on the link to start the download
-	document.body.removeChild(downloadLink); // Remove the link after triggering the download
+	const jsonString = JSON.stringify(obj, null, 2); 					// Convert object to JSON string
+	const blob = new Blob([jsonString], { type: 'application/json' });	// Create a Blob out of the string
+	const downloadLink = document.createElement('a');					// Create a download link for the blob
+	downloadLink.href = URL.createObjectURL(blob); 						// Use the Object URL as the href for the download link
+	downloadLink.download = `QOPUserData${formattedDateTime}.json`;		// Set the download attribute of the link to give the desired name to the file
+	document.body.appendChild(downloadLink);							// Append the link to the body
+	downloadLink.click();												// Programmatically trigger a click on the link to start the download
+	document.body.removeChild(downloadLink);							// Remove the link after triggering the download
 }
 
 export function ImportQOPUDJSON(QOPUserData: QOPUserDataTemplate) {
