fdd062f3c4f208ddc7dccdc18c2fb6d1a88669d4
JazzerThighs <profenjb199@gmail.com>
2023-08-22 17:17:29 -0400

    modified:   create-a-qwert-o-phone/src/WoodshedMode/Components/WoodshedUI.svelte 	deleted:    create-a-qwert-o-phone/src/WoodshedMode/gheufio.json 	modified:   create-a-qwert-o-phone/src/routes/+page.svelte

--- create-a-qwert-o-phone/src/WoodshedMode/Components/WoodshedUI.svelte
+++ create-a-qwert-o-phone/src/WoodshedMode/Components/WoodshedUI.svelte
@@ -1,44 +1,39 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
-    import { QOPUserData } from "../../UserDataMode/QOPUDStore";
-	import type { QOPUserDataTemplate } from "../../UserDataMode/initQOPUD";
-	import { DeltaSetValidator, OpenGutValidator } from "../../UserDataMode/validateQOPUD";
-	import { HydrateQOP, QOPTemplate } from "../initQOP";
-	import { QOPMutator } from "../mutateQOPLoop";
+	import { QOPUserData } from '../../UserDataMode/QOPUDStore';
+	import type { QOPUserDataTemplate } from '../../UserDataMode/initQOPUD';
+	import { DeltaSetValidator, OpenGutValidator } from '../../UserDataMode/validateQOPUD';
+	import { HydrateQOP, QOPTemplate } from '../initQOP';
+	import { QOPMutator } from '../mutateQOPLoop';
 
-    export let qop: QOPTemplate = new QOPTemplate();
-    const AudioContext = window.AudioContext;
-    const audioContext = new AudioContext();
+	export let qop: QOPTemplate = new QOPTemplate();
+	const AudioContext = window.AudioContext;
+	const audioContext = new AudioContext();
 
-    function WoodshedTriggerKeydown(event: KeyboardEvent) {
+	function WoodshedTriggerKeydown(event: KeyboardEvent) {
 		QOPMutator(event, 0, qop, audioContext);
 	}
 	function WoodshedTriggerKeyup(event: KeyboardEvent) {
 		QOPMutator(event, 1, qop, audioContext);
 	}
-	function WoodshedMode(QOPUserData: QOPUserDataTemplate, audioContext: AudioContext) {
-		DeltaSetValidator(QOPUserData);
-		OpenGutValidator(QOPUserData);
-		qop = HydrateQOP($QOPUserData, audioContext);
+	function WoodshedMode(userData: QOPUserDataTemplate, audioContext: AudioContext) {
+		DeltaSetValidator(userData);
+		OpenGutValidator(userData);
+		qop = HydrateQOP(userData, audioContext);
 		window.addEventListener('keydown', WoodshedTriggerKeydown);
 		window.addEventListener('keyup', WoodshedTriggerKeyup);
 	}
 
-    onMount(() => {
-        WoodshedMode($QOPUserData, audioContext);
+	onMount(() => {
+		WoodshedMode($QOPUserData, audioContext);
 		console.log(JSON.stringify(qop));
-    });
+	});
 
-    onDestroy(() => {
-        window.removeEventListener('keydown', WoodshedTriggerKeydown);
-        window.removeEventListener('keyup', WoodshedTriggerKeyup);
-    });
+	onDestroy(() => {
+		window.removeEventListener('keydown', WoodshedTriggerKeydown);
+		window.removeEventListener('keyup', WoodshedTriggerKeyup);
+	});
 </script>
 
-
-
-
-
 <style>
-
 </style>
--- create-a-qwert-o-phone/src/routes/+page.svelte
+++ create-a-qwert-o-phone/src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import Banner from './Banner.svelte';
 	import EditorUI from '../UserDataMode/Components/EditorUI.svelte';
 	import WoodshedUI from '../WoodshedMode/Components/WoodshedUI.svelte';
-	import { QOPUserData } from '../UserDataMode/QOPUDStore';
-	import type { QOPUserDataTemplate } from '../UserDataMode/initQOPUD';
-	import { QOPMutator } from '../WoodshedMode/mutateQOPLoop';
-	import { DeltaSetValidator, OpenGutValidator } from '../UserDataMode/validateQOPUD';
-	import { HydrateQOP, QOPTemplate } from '../WoodshedMode/initQOP';
 
 	let WoodshedModeToggle: boolean = false;
 	
